7487f3299af6aff00bb906bac8593d341e8793ad
hotovson <pepe.hotovson@gmail.com>
2018-04-13 11:39:58 +0200

    simpler formatter -> better output

--- lib/formatters/cucumber2.rb
+++ lib/formatters/cucumber2.rb
@@ -9,24 +9,5 @@ module Formatters
       @io.print "#{name.sub(/^\s*/, '').split("\n").first} "
       @io.flush
     end
-
-    def before_feature(_feature)
-      @exceptions = []
-      @start_time = Time.now
-    end
-
-    def after_feature(_feature)
-      print_elapsed_time @io, @start_time
-      @io.puts
-
-      @exceptions.each do |(exception, status)|
-        print_exception(exception, status, 2)
-      end
-    end
-
-    def exception(exception, status)
-      @exceptions << [exception, status]
-      super if defined?(super)
-    end
   end
 end
